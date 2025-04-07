da2462729f384a4a3ad1aa8601666cb428d4ab51
Israel <israelguerreroortiz83@gmail.com>
2025-04-07 13:01:50 +0200

    finished sheet03 JS

---
+++ JS/sheet03/ex05/script.js
@@ -0,0 +1,8 @@
+// Función normal
+const numeros = [1, 2, 3];
+const dobles = numeros.map(function (n) {
+    return n * 2;
+});
+// Función de flecha
+
+const arrowDobles = numeros.map((n) => n * 2);
