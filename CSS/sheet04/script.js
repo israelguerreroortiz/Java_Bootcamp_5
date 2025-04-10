07b49b1022aba950b7f7df71e7140eb99d18702c
IsraelGuerrero <israelguerreroortiz83@gmail.com>
2025-04-10 18:34:07 +0200

    get pokemon info(names, img, etc..) from api

--- CSS/sheet04/script.js
+++ CSS/sheet04/script.js
@@ -6,14 +6,34 @@ async function getPokemonsData() {
     const pokemonDetails = await Promise.all(
         pokemons.map(async (pokemon) => {
             const pokemonResponse = await fetch(pokemon.url);
-            console.log(pokemonResponse);
             return pokemonResponse.json();
         })
     );
-    return pokemonDetails
+    const pokemonNames = pokemonDetails.map(pokemon => pokemon.name);
+    const pokemonImages = pokemonDetails.map(pokemon => pokemon.sprites.front_default);
+    const pokemonID = pokemonDetails.map(pokemon => pokemon.id);
+    const pokemonTypes = pokemonDetails.map(pokemon => pokemon.types.map(typeInfo => typeInfo.type.name));
+    return {pokemonNames, pokemonImages, pokemonID, pokemonTypes};
 };
 
 // img id nombre tipos evolucion
+const pokemonEvolutions = async () => {
+    const urlSpecies = 'https://pokeapi.co/api/v2/pokemon-species?limit=20';
+    const speciesResponse = await fetch(urlSpecies);
+    const species = await speciesResponse.json();
+    const speciesDetails = await Promise.all(
+        species.results.map(async (speciesItem) => {
+            const speciesResponse = await fetch(speciesItem.url);
+            return speciesResponse.json();
+        })
+    );
+    const speciesEvo = speciesDetails.map(specie => specie.evolves_from_species);
+    return speciesEvo;
+};
 
-getPokemonsData();
+const renderPokemonCards = async () => {
+    const {pokemonNames, pokemonImages, pokemonID, pokemonTypes} = await getPokemonsData();
+    
+}
+renderPokemonCards()
   
